f4ba9b9de493e81e33b0d48f8f872f3d4b6c424b
Ahmed <m.ahmed.arif>
2025-04-03 10:22:35 +0200

    Initial Commit

---
+++ Focustime/FocustimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocustimeApp.swift
+//  Focustime
+//
+//  Created by Ahmad Arif on 03.04.25.
+//
+
+import SwiftUI
+
+@main
+struct FocustimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
